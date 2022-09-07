d4ad53732354db9a60d2398490bc531ccc4d3702
Mariam <adesinamariam7@gmail.com>
2022-09-07 02:43:50 +0200

    try

--- gen.js
+++ gen.js
@@ -13,16 +13,19 @@ fetch('https://randomuser.me/api/?results=100')
                 </div>
             </div>
             <div class="col-lg-12 col-md-7 col-sm-7 my-auto">
-                <h3 id="name"><span>Name:</span>${data.results[i].name.title} ${data.results[i].name.first} ${data.results[i].name.last} </h3>
-                <h3 id="address"><span>Address:</span>${data.results[i].location.state} ${data.results[i].location.country}</h3>
-                <h3 id="date"><span>DOB:</span>${data.results[i].dob.date} ${data.results[i].dob.age}</h3>
-                <h3 id="age"><span>Age:</span>${data.results[i].dob.age}</h3>
-                <h3 id="location"><span>Email:</span>${data.results[i].email}</h3>
-                <h3 id="number"><span>Number:</span>${data.results[i].phone}</h3>
-                <h3 id="password"><span>Password:</span>${data.results[i].login.password}</h3>
+                <h3 id="name"><span><i class="far fa-user" id="user" onmouseover="show()"></i></span>${data.results[i].name.title} ${data.results[i].name.first} ${data.results[i].name.last} </h3>
+                <h3 id="address"><span><i class="fas fa-envelope" id="msg" onmouseover="show1()"></i></span>${data.results[i].location.state} ${data.results[i].location.country}</h3>
+                <h3 id="date"><span><i class="fas fa-calendar-alt" id="date" onmouseover="show2()"></i></span>${data.results[i].dob.date} ${data.results[i].dob.age}</h3>
+                <h3 id="age"><span><i class="fas fa-map-marked" id="location" onmouseover="show3()"></i></span>${data.results[i].dob.age}</h3>
+                <h3 id="location"><span><i class="fas fa-envelope" id="msg" onmouseover="show4()"></i></span>${data.results[i].email}</h3>
+                <h3 id="number"><span><i class="fas fa-phone-alt" id="number" onmouseover="show5()"></i></span>${data.results[i].phone}</h3>
+                <h3 id="password"><span> <i class="fas fa-lock" id="password" onmouseover="show6()"></i></span>${data.results[i].login.password}</h3>
             </div>
         </div>`
     }
     userInfo.innerHTML += display;
 })
+function show(){
+    document.getElementById('userInfo').innerHTML =  display;
+}
 
